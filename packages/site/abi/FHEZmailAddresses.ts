9cd2ba497301ee3176e8d273e237d2047bff1b50
“narutoLab” <naruto07061998@gmail.com>
2025-08-31 17:24:14 +0700

    feature: update contract address and rpc

--- packages/site/abi/FHEZmailAddresses.ts
+++ packages/site/abi/FHEZmailAddresses.ts
@@ -1,6 +1,6 @@
 export const FHEZmailAddresses: Record<string, { address: string; chainId: number; chainName: string }> = {
   "11155111": {
-    address: "0xD51b4FDb44e5E18C51a7052E20228873913085fD",
+    address: "0xCB45011ca6B8CDce01792CBB9B4c999479E94a0E",
     chainId: 11155111,
     chainName: "sepolia",
   },
